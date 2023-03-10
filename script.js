da3fae0a14370f6415f86de768935074a7988c49
Gurbax <hi@gurbax.lol>
2023-03-10 10:04:54 +0530

    Add setTraits fn

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-let physicalTrait = "";
-let vocalTrait = "";
-
 const allPhysicaTraits = {
     "speed": ["fast", "slow"],
     "behaviour": ["fidgety", "still"],
     "movement": ["legato", "staccato"]
 }
 
-let randomTrait = function (obj) {
-    let keys = Object.keys(obj);
+const allVocalTraits = {
+    "accent": ["british", "french", "malayali", "punjabi", "italian", "tapori"],
+    "pitch": ["low", "high"],
+    "talker": ["fast", "slow"]
+}
+
+function randomTrait(obj) {
+    let keys = Object.keys(obj);  // All keys in obj
     random_key = keys[Math.floor(Math.random() * keys.length)]
     random_value = obj[random_key][Math.floor(Math.random() * obj[random_key].length)]
-    output = `${random_value} ${random_key}`
-    return output;
+    return `${random_value} ${random_key}`;
 };
 
-document.getElementById("physical").innerHTML = randomTrait(allPhysicaTraits);
-document.getElementById("vocal").innerHTML = vocalTrait;
+function setTraits() {
+    document.getElementById("physical").innerHTML = randomTrait(allPhysicaTraits);
+    document.getElementById("vocal").innerHTML = randomTrait(allVocalTraits);
+}
+
+setTraits()
